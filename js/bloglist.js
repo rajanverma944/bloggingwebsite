0e2e65bf31431d2415ca2d16f47da3e3f3fd7c49
rajan verma <shivanshawesome31@gmail.com>
2019-03-14 04:17:50 +0700

    changes made to header.css header.js for part A and part b added

---
+++ js/bloglist.js
@@ -0,0 +1,17 @@
+function confirmremove()
+{
+document.getElementById('deletemodal').style.display='none';
+}
+function removemodal()
+{
+document.getElementById('deletemodal').style.display='none';
+}
+function deletepost()
+{
+document.getElementById('deletemodal').style.display='block';
+}
+function open1()
+{
+window.location='post.html';
+
+}
---
+++ js/header.js
@@ -0,0 +1,29 @@
+function redirect()
+{
+document.getElementById('modalwrapper').style.display='block';
+}
+function signup()
+{
+document.getElementById('modalwrapper').style.display='block';
+}
+function signin()
+{
+document.getElementById('modalwrapper2').style.display='block';
+}
+function redirect2()
+{
+window.location='html/bloglist.html';
+}
+function closesignup()
+{
+
+document.getElementById('modalwrapper').style.display='none';
+}
+
+
+function closesignin()
+{
+
+document.getElementById('modalwrapper2').style.display='none';
+}
+
